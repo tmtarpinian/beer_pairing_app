d9a969e36e3deedf142afb8139b97ada8513b74a
Trevor Tarpinian <tmtarpinian@gmail.com>
2020-04-16 19:10:56 +0000

    begin writing Api class

--- lib/beers/Api.rb
+++ lib/beers/Api.rb
@@ -1,5 +1,20 @@
 class Api
-  
-  
-  
+
+  def self.get_beer(food)
+
+    url = "https://api.punkapi.com/v2/beers?food=#{food}&per_page=80"
+    response = Net::HTTP.get(URI(url))
+    #binding.pry
+    beers = JSON.parse(response)
+        beers.each do |b| 
+        new_beer = Beer.find_or_create_by_name(b["name"], b["description"], b["food_pairing"], b["abv"])
+       
+      end
+
+    end
+
+  end
+
+
+ 
 end
