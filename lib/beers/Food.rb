0505c0553cdbdc0097073bd7c8ac33d2a68e18a7
Trevor Tarpinian <tmtarpinian@gmail.com>
2020-04-16 19:15:18 +0000

    write find class methods in Food model

--- lib/beers/Food.rb
+++ lib/beers/Food.rb
@@ -1,11 +1,34 @@
 class Food
-  
-  attr_accessor
-  
+
+  attr_accessor :name, :beers
+
   @@all = []
-  
-  def initialize
-  
+
+  def initialize(name)
+    @name = name
+    @beers = []
+    @@all << self
   end
-  
+
+  def self.all
+    @@all
+  end
+
+  def self.create(food)
+    food = self.new(food)
+  end
+
+  def self.find_or_create_by_name(name)
+    food = self.find_by_name(name)
+    if food
+      food
+    else
+      self.create(name) 
+    end
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|i| i.name == name}
+  end
+    
 end
