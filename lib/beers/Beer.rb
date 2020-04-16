1653544e6bab94770e6c53be4489bc27d1dfa863
Trevor Tarpinian <tmtarpinian@gmail.com>
2020-04-16 19:17:10 +0000

    write find class methods in Beer  model

--- lib/beers/Beer.rb
+++ lib/beers/Beer.rb
@@ -1,11 +1,28 @@
 class Beer
-  
-  attr_accessor
-  
+
+  attr_accessor :name, :description, :meals, :abv, :foods
+
   @@all = []
-  
-  def initialize
-  
+
+  def initialize (name, description, meals, abv)
+    @name = name
+    @description = description
+    @meals = meals.join(", ")
+    @abv = abv
+    @foods = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.create(name, description, meals, abv)
+    beer = self.new(name, description, meals, abv)
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|i| i.name == name}
   end
   
 end
