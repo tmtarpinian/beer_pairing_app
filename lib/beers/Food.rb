0d8f91365863a91f72ef5dfc638f415ee7815f74
Trevor Tarpinian <tmtarpinian@gmail.com>
2020-04-16 21:47:10 +0000

    review syntax in Food model

--- lib/beers/Food.rb
+++ lib/beers/Food.rb
@@ -1,34 +1,40 @@
 class Food
-
-  attr_accessor :name, :beers
-
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @beers = []
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.create(food)
-    food = self.new(food)
-  end
-
-  def self.find_or_create_by_name(name)
-    food = self.find_by_name(name)
-    if food
-      food
-    else
-      self.create(name) 
-    end
-  end
-
-  def self.find_by_name(name)
-    self.all.find{|i| i.name == name}
-  end
     
+    attr_accessor :name, :beers
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @beers = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+
+    def self.create(food)
+        food = self.new(food)
+    end
+
+
+
+    def self.find_or_create_by_name(name)
+        food = self.find_by_name(name)
+        if food
+        food
+        else
+        self.create(name) 
+        end
+    end
+
+  
+
+    def self.find_by_name(name)
+        self.all.find{|i| i.name == name}
+    end
+
 end
