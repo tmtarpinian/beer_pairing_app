1c732841f76a690914e43bc1e5169a1af2fcb9da
Trevor Tarpinian <tmtarpinian@gmail.com>
2020-04-16 19:32:48 +0000

    write first printer method in Cli model

--- lib/beers/Cli.rb
+++ lib/beers/Cli.rb
@@ -20,14 +20,25 @@ class Cli
   
   
     def prompt
-        puts " "
-        puts "Type a number and hit Enter/Return to see a Beer's details, including meal ideas."
-        puts " "
-        puts "Type the word 'list' and hit Enter/Return to see your most recent search results again."
-        puts " "
-        puts "Type the word 'food' and hit Enter/Return to search by a different food"
-        puts " "
-        puts "Type the word 'exit' and hit Enter/Return to leave the app."
-        puts " "
+      puts " "
+      puts "Type a number and hit Enter/Return to see a Beer's details, including meal ideas."
+      puts " "
+      puts "Type the word 'list' and hit Enter/Return to see your most recent search results again."
+      puts " "
+      puts "Type the word 'food' and hit Enter/Return to search by a different food"
+      puts " "
+      puts "Type the word 'exit' and hit Enter/Return to leave the app."
+      puts " "
+    end
+    
+    def print_beers_by_food(food)
+      puts " "
+      puts "If you're making a meal with the food '#{food.name}', consider pairing with the following beers:"
+      puts " "
+      results = Beer.find_by_food(food)
+      results.each.with_index(1) do |beer, i|
+      puts "#{i}. #{beer.name}"
+      puts " "
+      end
     end
 end
