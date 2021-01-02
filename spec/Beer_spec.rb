ef0aec410c9e12bbf80da28d37ee66720946c3eb
ferrisbueller66 <tmtarpinian@gmail.com>
2021-01-02 14:42:07 -0500

    require environment.rb in spec_helper; write fbeer instance initialize unit test in beer_spec

--- spec/Beer_spec.rb
+++ spec/Beer_spec.rb
@@ -7,7 +7,7 @@ describe "Beer" do
     it "accepts a name, description, array of meals, and abv for the new beer" do
       new_beer = Beer.new("Tank 7", "Fruity Aromatics, citrusy balance, dry and hoppy perfection", ["Raspberry Tort", "Chile Lime Quesadillas"], "8.5%")
 
-      new_beer_name = new_song.instance_variable_get(:@name)
+      new_beer_name = new_beer.instance_variable_get(:@name)
 
       expect(new_beer_name).to eq("Tank 7")
     end
