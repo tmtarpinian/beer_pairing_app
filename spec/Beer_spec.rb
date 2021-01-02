3656fd7207336cac6bc9b3884e3d9f546b714c98
ferrisbueller66 <tmtarpinian@gmail.com>
2021-01-02 14:26:07 -0500

    add rspec to gemfile, add spec directory, begin writing unit tests for beer model

---
+++ spec/Beer_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe "Beer" do
+  let(:song) { Song.new("In the Aeroplane Over the Sea") }
+
+  describe "#initialize" do
+    it "accepts a name, description, array of meals, and abv for the new beer" do
+      new_beer = Beer.new("Tank 7", "Fruity Aromatics, citrusy balance, dry and hoppy perfection", ["Raspberry Tort", "Chile Lime Quesadillas"], "8.5%")
+
+      new_beer_name = new_song.instance_variable_get(:@name)
+
+      expect(new_beer_name).to eq("Tank 7")
+    end
+  end
+end
